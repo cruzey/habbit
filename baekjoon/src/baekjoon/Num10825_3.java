2ed56b2db4c5a0ad9b735dd81d88fa5850770de4
cruzey <cruzey0205@gmail.com>
2026-01-26 10:04:10 +0900

    10825

---
+++ baekjoon/src/baekjoon/Num10825_3.java
@@ -0,0 +1,51 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Num10825_3 {
+
+    private record StudentInfo (String name, 
+        Integer mathScore,
+        Integer koreanScore,
+        Integer englishScore) {
+    }
+
+    public static void main(String[] args) throws IOException{
+
+        List<StudentInfo> sList = new ArrayList<>();
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int num = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
+
+        for (int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            StudentInfo student = new StudentInfo(st.nextToken(), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            sList.add(student);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        
+        sList.stream()
+             .sorted(
+                Comparator.comparing(StudentInfo::koreanScore, Comparator.reverseOrder()) // 1순위: 국어 내림차순
+                    .thenComparing(StudentInfo::englishScore)                             // 2순위: 영어 오름차순 (기본)
+                    .thenComparing(StudentInfo::mathScore, Comparator.reverseOrder())     // 3순위: 수학 내림차순
+                    .thenComparing(StudentInfo::name)                                     // 4순위: 이름 사전순 (기본)
+             )
+             .forEach(student -> sb.append(student.name()).append("\n")); // 출력
+
+        System.out.println(sb);
+        
+    }
+
+}
