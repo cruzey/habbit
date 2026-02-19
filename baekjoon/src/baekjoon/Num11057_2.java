5c31e377f7c0c3af9d4d8e6f8bd9930264b456b2
cruzey <cruzey0205@gmail.com>
2026-02-20 08:44:38 +0900

    11057

---
+++ baekjoon/src/baekjoon/Num11057_2.java
@@ -0,0 +1,46 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Num11057_2 {
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(solve(N));
+
+    }
+
+    static int solve (int N) {
+
+        int[][] dp = new int[N+1][10];
+        //dp[N] = 길이가 N인 오르막 수의 개수
+        //dp[1][j] = 10, dp[2][j] = 55
+        //dp[i][j] = dp[i-1][0....j]
+        //dp[i][j-1] = dp[i-1][0...j-1]
+        //즉 dp[i][j] = dp[i][j-1] + dp[i-1][j] !!!!!!!!!!!!!!!
+
+        //dp[1][0...9] 0으로 초기화
+        for (int j = 0; j < 10; j++) {
+            dp[1][j] = 1;
+        }
+
+        for (int i = 2; i < N + 1; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) dp[i][j] = dp[i-1][j] % 10007;
+                else dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 10007;
+            }
+        }
+
+        int result = 0;
+        for (int j = 0; j < 10; j++) {
+            result = (result + dp[N][j]) % 10007;
+        }
+
+        return result;
+    }
+}
