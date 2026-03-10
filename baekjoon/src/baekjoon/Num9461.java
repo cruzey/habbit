b13f6dbfdaf2ff6e1cd3c44c140ee2737f5f2e9a
cruzey <cruzey0205@gmail.com>
2026-03-11 08:39:36 +0900

    9461 +

---
+++ baekjoon/src/baekjoon/Num9461.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Num9461 {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(solve(N)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static long solve(int N) {
+
+        long[] P = new long[N + 5];
+
+        P[1] = 1;
+        P[2] = 1;
+        P[3] = 1;
+        P[4] = 2;
+        P[5] = 2;
+        for (int i = 6; i < N + 1; i++) {
+            P[i] = P[i - 1] + P[i - 5];
+        }
+        
+        return P[N];
+    }
+}
